3ef4032f8865d737ea31c97d7d47fabdd5aab47a
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2019-02-24 22:47:24 +0200

    solution for large inputs

--- the-grid-search.js
+++ the-grid-search.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 //Input example
 // 1234567890
 // 0987654321
@@ -10,62 +11,71 @@
 // 111111
 
 //Answer: YES
-const converArrayOfStringsTo2DArray = (arr) => arr.reduce((acc, curr) => [...acc, curr.split('')], []);
+const converArrayOfStringsTo2DArray = arr =>
+  arr.reduce((acc, curr) => [...acc, curr.split("")], []);
 
 const isEqual2DArrays = (arr, arr2) => {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i].join('') !== arr2[i].join('')) {
+    if (!arr2[i]) {
       return false;
     }
-  }
+    for (let j = 0; j < arr[i].length; j++) {
+      if (!arr2[i][j]) {
+        return false;
+      }
 
-  return true;
-}
-
-const getSubMatrix = (arr, numRows, numColumns, currRow, currColumn) => {
-  let result = [];
-
-  for (let i = currRow; i < arr.length - numRows; i++) {
-    result.push(arr[i].slice(currColumn, currColumn + numColumns));
-  }
-
-  return converArrayOfStringsTo2DArray(result);
-}
-
-function gridSearch(G, P) {
-  const numColumns = P[0].length;
-  const numRows = P.length;
-  // step convert array of strings to 2d arrays
-  const [gAS2DArray, pAS2DArray] = [G, P].map(converArrayOfStringsTo2DArray);
-
-  for (let i = 0; i < gAS2DArray.length - numRows; i++) {
-    for (let j = 0; j < gAS2DArray[i].length - numColumns; j++) {
-      const subMtx = getSubMatrix(G, numRows, numColumns, i, j);
-      if (isEqual2DArrays(pAS2DArray, subMtx)) {
-        return 'YES';
+      if (arr2[i][j] !== arr[i][j]) {
+        return false;
       }
     }
   }
 
-  return 'NO'
+  return true;
+};
+
+const getSubMatrix = (arr, numRows, numColumns, currRow, currColumn) => {
+  let result = [];
+
+  for (let i = currRow; i < currRow + numRows; i++) {
+    result.push(arr[i].slice(currColumn, currColumn + numColumns));
+  }
+
+  return converArrayOfStringsTo2DArray(result);
+};
+
+function gridSearch(G, P) {
+  const numColumns = P[0].length;
+  const numRows = P.length;
+
+  // step convert array of strings to 2d arrays
+  const [gAS2DArray, pAS2DArray] = [G, P].map(converArrayOfStringsTo2DArray);
+
+  for (let i = 0; i <= gAS2DArray.length - numRows; i++) {
+    for (let j = 0; j <= gAS2DArray[i].length - numColumns; j++) {
+      if (gAS2DArray[i][j] === pAS2DArray[0][0]) {
+        const subMatrix = getSubMatrix(G, numRows, numColumns, i, j);
+        if (isEqual2DArrays(pAS2DArray, subMatrix)) {
+          return "YES";
+        }
+      }
+    }
+  }
+
+  return "NO";
 }
 
+const [GDATA, PDATA] = ["input1.txt", "input2.txt"]
+  .map(path => fs.readFileSync(path))
+  .map(String)
+  .map(data => data.split("\n"));
 
-const G = [
-  '7283455864',
-  '6731158619',
-  '8988242643',
-  '3830589324',
-  '2229505813',
-  '5633845374',
-  '6473530293',
-  '7053106601',
-  '0834282956',
-  '4607924137'
-];
+console.log(GDATA);
+console.log(PDATA);
 
-const P = ['9505', '3845', '3530'];
+// const G = GDATA.split("\n");
+// console.log(G);
 
+const P = ["115", "116"];
 
-console.log(gridSearch(G, P));
+console.log(gridSearch(GDATA, PDATA));
 // console.log(getSubMatrix(G, 5, 6, 1, 1));
