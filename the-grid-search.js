b7357586be1948afa4d49e0fda485ff20bfdb30a
vadim_nicolaev_v <vadim.nicolaev@vitamojo.com>
2019-03-05 23:00:02 +0200

    4X nested loops with correct breaks faster than 2X with joining string and math them

--- the-grid-search.js
+++ the-grid-search.js
@@ -14,51 +14,27 @@ const fs = require("fs");
 const converArrayOfStringsTo2DArray = arr =>
   arr.reduce((acc, curr) => [...acc, curr.split("")], []);
 
-const isEqual2DArrays = (arr, arr2) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (!arr2[i]) {
-      return false;
-    }
-    for (let j = 0; j < arr[i].length; j++) {
-      if (!arr2[i][j]) {
-        return false;
-      }
-
-      if (arr2[i][j] !== arr[i][j]) {
-        return false;
-      }
-    }
-  }
-
-  return true;
-};
-
-const getSubMatrix = (arr, numRows, numColumns, currRow, currColumn) => {
-  let result = [];
-
-  for (let i = currRow; i < currRow + numRows; i++) {
-    result.push(arr[i].slice(currColumn, currColumn + numColumns));
-  }
-
-  return converArrayOfStringsTo2DArray(result);
-};
-
 function gridSearch(G, P) {
   const numColumns = P[0].length;
   const numRows = P.length;
+  const pASString = P.join("");
 
   // step convert array of strings to 2d arrays
   const [gAS2DArray, pAS2DArray] = [G, P].map(converArrayOfStringsTo2DArray);
 
   for (let i = 0; i <= gAS2DArray.length - numRows; i++) {
     for (let j = 0; j <= gAS2DArray[i].length - numColumns; j++) {
-      const firstLine = gAS2DArray[i].join('').slice(j, j + numColumns);
-      if (firstLine === P[0]) {
-        const subMatrix = getSubMatrix(G, numRows, numColumns, i, j);
-        if (isEqual2DArrays(pAS2DArray, subMatrix)) {
-          return "YES";
+      let flag = true;
+      for (let k = 0; k < pAS2DArray.length; k++) {
+        if (!flag) break;
+        for (let m = 0; m < pAS2DArray[k].length; m++) {
+          if (pAS2DArray[k][m] !== gAS2DArray[i + k][j + m]) {
+            flag = false;
+            break;
+          }
         }
       }
+      if (flag) return "YES";
     }
   }
 
@@ -66,7 +42,7 @@ function gridSearch(G, P) {
 }
 
 const [GDATA, PDATA] = ["input1.txt", "input2.txt"]
-.map(path => fs.readFileSync(path))
+  .map(path => fs.readFileSync(path))
   .map(String)
   .map(data => data.split("\n"));
 
