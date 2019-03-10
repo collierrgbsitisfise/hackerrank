965e771edc3208a5e6631778ea30e7de7fda89f8
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2019-03-10 22:33:45 +0200

    solution by editorial

--- absolute-permutation.js
+++ absolute-permutation.js
@@ -1,45 +1,20 @@
 function absolutePermutation(n, k) {
-  const setInput = new Array(n).fill(0).map((_, i) => ++i);
-  const corectPerm = getPermutations(setInput, k);
-
-  if (corectPerm) {
-    return corectPerm;
+  let vector = [];
+  if (!k) {
+    return Array(n)
+      .fill(0)
+      .map((_, i) => ++i);
   }
 
-  return -1;
-}
-
-const isAbsolutePermutation = (arr, k) => {
-  let isAbsolute = true;
-  for (let i = 0; i < arr.length; i++) {
-    if (Math.abs(arr[i] - i - 1) - k !== 0) {
-      return false;
+  if (n % k == 0 && (n / k) % 2 == 0) {
+    for (let i = 1; i <= n; i++) {
+      vector.push(i + (Math.floor((i - 1) / k) % 2 ? -k : k));
     }
+  } else {
+    return [-1];
   }
 
-  return isAbsolute;
+  return vector;
 }
 
-const getPermutations = (arr, k) => {
-  let res = false;
-  const permutations = (arr, permut = []) => {
-    if (!arr.length && isAbsolutePermutation(permut, k)) {
-      res = permut;
-      return;
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-      permut = [...permut, arr[i]];
-      permutations([...arr.slice(0, i), ...arr.slice(i + 1)], permut);
-      permut = permut.slice(0, permut.length - 1);
-    }
-  };
-
-  permutations(arr);
-
-  return res;
-}
-
-
-
-console.log(absolutePermutation(10, 5));
+console.log(absolutePermutation(2, 1));
