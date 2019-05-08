56d2ca93c620730384a79b2354466c5775ac8829
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2019-05-09 00:57:42 +0300

    resolve it using properies of prefix sum algo

--- IPK/Arrays/arrayManipulation.js
+++ IPK/Arrays/arrayManipulation.js
@@ -1,20 +1,20 @@
 // n: 5;
 // queries: [[1, 2, 100], [2, 5, 100], [3, 4, 100]];
 // Expected Output: 200
+//HINT here is used prefix sum algo to decrease time complexety from O(N * M) -> O(N)
+
 function arrayManipulation(n, queries) {
   let max = 0;
-  let hashMapHelper = {};
-  for (let query of queries) {
-    for (let i = query[0]; i <= query[1]; i++) {
-      const prevValue = hashMapHelper[i];
-      const newVlaue =
-        typeof hashMapHelper[i] === "undefined"
-          ? query[2]
-          : prevValue + query[2];
+  const arr = new Array(n + 2).fill(0);
 
-      max = Math.max(max, newVlaue);
-      hashMapHelper[i] = newVlaue;
-    }
+  for (let query of queries) {
+    arr[query[0]] += query[2];
+    arr[query[1] + 1] -= query[2];
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] + (i !== 0 ? arr[i - 1] : 0);
+    max = Math.max(max, arr[i]);
   }
 
   return max;
